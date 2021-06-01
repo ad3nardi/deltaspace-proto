110e775e3a667f708190b2add325907926c34b96
AD3Nardi <ad3nardi@gmail.com>
2021-06-02 00:11:32 +1000

    SetUpCont

--- Assets/Scripts/PlayerCont.cs
+++ Assets/Scripts/PlayerCont.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class PlayerCont : InputObj
 {
     public bool inAction;
 
     //Movement
-    public float moveSpeed;
-    public float rollSpeed;
-    public float yawSpeed;
-    public float pitchSpeed;
+    public float moveSpd;
+    public float throttle;
+    public float latSpd;
+    public float rollSpd;
+    public float yawSpd;
+    public float pitchSpd;
 
     public bool isPitchingUp;
     public bool isPitchingDown;
@@ -28,20 +31,18 @@ public class PlayerCont : InputObj
     void Start()
     {
         cc = gameObject.GetComponent<CharacterController>();
-        inAction = gameObject.GetComponent<PlayerHealth>().inAction;
+        inAction = true;
     }
 
     public override void GetInputs(InputList inputs)
     {
-        //Debug.Log("vfgf");
         currentInput = inputs;
     }
 
     public override void Tick(InputList inputs, float delta)
     {
-        directionVector = directionVector.normalized;
-        transform.rotation = Quaternion.LookRotation(directionVector);
-        cc.Move(movementVector * Time.deltaTime * moveSpeed);
+     //   directionVector = directionVector.normalized;
+     //   transform.rotation = Quaternion.LookRotation(directionVector);
     }
 
     public override void FixedTick(float delta)
@@ -51,19 +52,37 @@ public class PlayerCont : InputObj
             if(currentInput.vertical < 0 && isPitchingDown != true)
             {
                 isPitchingUp = true;
-                transform.rotation = Quaternion.LookRotation(directionVector);
+                gameObject.transform.Rotate(pitchSpd * Time.deltaTime, 0.0f, 0.0f);
+                cc.Move(Vector3.down * latSpd * Time.deltaTime);
             }
             if (currentInput.vertical > 0 && isPitchingUp != true)
             {
                 isPitchingDown = true;
+                gameObject.transform.Rotate(-pitchSpd * Time.deltaTime, 0.0f, 0.0f);
+                cc.Move(Vector3.up * latSpd * Time.deltaTime);
+            }
+            if(currentInput.vertical == 0)
+            {
+                isPitchingUp = false;
+                isPitchingDown = false;
             }
             if (currentInput.horizontal < 0)
             {
                 isYawingLeft = true;
+                gameObject.transform.Rotate(0.0f, 0.0f, -rollSpd * Time.deltaTime);
+                cc.Move(Vector3.left * latSpd * Time.deltaTime);
+
             }
             if (currentInput.horizontal > 0)
             {
                 isYawingRight = true;
+                gameObject.transform.Rotate(0.0f, 0.0f, -rollSpd * Time.deltaTime);
+                cc.Move(Vector3.right * latSpd * Time.deltaTime);
+            }
+            if (currentInput.horizontal == 0)
+            {
+                isYawingLeft = false;
+                isYawingRight = false;
             }
             if (currentInput.verticalAim < 0)
             {
@@ -87,16 +106,25 @@ public class PlayerCont : InputObj
             }
             if (currentInput.rollLeft == true)
             {
-
+                gameObject.transform.Rotate(0.0f, 0.0f, rollSpd * Time.deltaTime);
             }
             if (currentInput.rollRight == true)
             {
+                gameObject.transform.Rotate(0.0f, 0.0f, -rollSpd * Time.deltaTime);
 
             }
-            float rotX = currentInput.horizontal;
-            float rotZ = -currentInput.vertical;
+            cc.Move(Vector3.forward * moveSpd * Time.deltaTime);
+
+        //    if(gameObject.transform.rotation)
+        //    {
+        //      gameObject.transform.Rotate(0.0f, 0.0f, 0.0f)
+        //    }
+
+          /* 
             directionVector = new Vector3(rotX, 0.0f, rotZ);
-            /*
+            var angle = Mathf.Atan2(directionVector.x, directionVector.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, angle, 0f), Time.fixedDeltaTime * pitchSpd);
+            
             if(directionVector.magnitude != 0)
             {
                 directionVector = Vector3.zero;
--- Assets/Scripts/PlayerWeapons.cs
+++ Assets/Scripts/PlayerWeapons.cs
@@ -1,18 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerWeapons : MonoBehaviour
+public class PlayerWeapons : InputObj
 {
-    // Start is called before the first frame update
+    InputList currentInput = new InputList();
+    public bool inAction;
+    public bool isFiring;
+    public int weaponType;
+
+    //weaponType mg = 1, missile = 2, torpedo = 3
+    public GameObject mg;
+    public GameObject missile;
+    public GameObject torp;
+    public GameObject torpLaunch;
+
+    public float torpPower;
+
+    public GameObject raycastMarker = null;
+
+    public float mgFireRate;
+    private bool mgReadyFire;
+
     void Start()
     {
-        
+        inAction = true;
+        weaponType = 1;
+    }
+    public override void GetInputs(InputList inputs)
+    {
+        currentInput = inputs;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void FixedTick(float delta)
     {
+        RaycastHit hit;
+        float distanceOfRay = 100;
+
+        if (inAction == true)
+        {
+            if (currentInput.fire == true && weaponType == 1)
+            {
+                isFiring = true;
+                raycastMarker.transform.position = hit.point;
+                raycastMarker.GetComponentInChildren<ParticleSystem>().Play();
+                Debug.Log(hit.point);
+
+                if (mgReadyFire == false)
+                {
+                    return;
+                }
+
+                mgReadyFire = false;
+                Invoke("ResetMG", mgFireRate);
+            }
+
+
+            if (currentInput.fire == true && weaponType == 2)
+            {
+                isFiring = true;
+            }
+            if (currentInput.fire == true && weaponType == 3)
+            {
+                GameObject GO = Instantiate(torp, torpLaunch.transform.position, Quaternion.identity) as GameObject;
+                GO.GetComponent<Rigidbody>().AddForce(torpLaunch.transform.forward * torpPower, ForceMode.Impulse);
+            }
+        }
+
         
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * distanceOfRay);
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, distanceOfRay))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+    private void ResetMG()
+    {
+        mgReadyFire = true;
     }
 }
