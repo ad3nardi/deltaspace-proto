07e3a83ed5ed413dd6ecf2a96298405242ddbd89
Ad3nardi <ad3nardi@gmail.com>
2021-07-13 14:42:06 +1000

    audio

---
+++ Assets/Scripts/BasicPlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlay : MonoBehaviour
+{
+    public AudioSource aS;
+    public void PlaySound()
+    {
+        aS.Play();
+    }
+    
+}
